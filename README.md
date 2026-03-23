c4c49062592e61a009ef0d833c57d0ee69de4f54
siddGaurav <gauravsiddharth.mj@gmail.com>
2026-03-23 13:19:26 +0530

    fix: jwt typescript error and render deploy fix

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ git remote add origin https://github.com/siddGaurav/typescript.git
 git branch -M main
 git push -u origin main
 # node --loader ts-node/esm src/index.ts
-
+# node --inspect-brk -r ts-node/esm src/index.ts
+# node --inspect-brk ./node_modules/.bin/tsx src/index.ts
+# node --inspect ./node_modules/.bin/tsx src/index.ts
 echo "# typescript" >> README.md
 git init
 git add README.md
@@ -12,3 +14,24 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/siddGaurav/typescript.git
 git push -u origin main
+
+
+
+{
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "type": "node",
+      "request": "launch",
+      "name": "Debug TSX",
+      "runtimeExecutable": "node",
+      "runtimeArgs": [
+        "./node_modules/.bin/tsx"
+      ],
+      "args": ["src/index.ts"],
+      "cwd": "${workspaceFolder}",
+      "sourceMaps": true,
+      "skipFiles": ["<node_internals>/**"]
+    }
+  ]
+}
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,28 +1,42 @@
+import jwt, { SignOptions } from "jsonwebtoken";
 
-import jwt from 'jsonwebtoken';
+const JWT_EXPIRE = process.env.JWT_EXPIRE || "7d";
 
-const JWT_EXPIRE = process.env.JWT_EXPIRE || '7d';
-const getJwtSecret = ():string => process.env.JWT_SECRET || 't6d_6Gf^2**145@62$$&1kH@';
-
-export interface TokenPayload {
-    id: number;
-    email: string;
-}
-
-export function signJwt(payload: TokenPayload, expiresIn?: string | number){
-    try {
-        const secret = getJwtSecret();
-        return jwt.sign(payload, secret, { expiresIn: expiresIn || JWT_EXPIRE });
-    } catch (err) {
-        return null;
-    }
+const getJwtSecret = (): string => {
+  return process.env.JWT_SECRET || "t6d_6Gf^2**145@62$$&1kH@";
 };
 
+export interface TokenPayload {
+  id: number;
+  email: string;
+}
+
+
+
+export function signJwt(
+  payload: TokenPayload,
+  expiresIn?: string | number
+) {
+  try {
+    const secret = getJwtSecret();
+
+    const exp = (expiresIn ?? JWT_EXPIRE ?? "1d") as NonNullable<SignOptions["expiresIn"]>;
+
+    const options: SignOptions = {
+      expiresIn: exp
+    };
+
+    return jwt.sign(payload, secret, options);
+  } catch (err) {
+    return null;
+  }
+}
+
 export const verifyJwt = <T = any>(token: string): T | null => {
-    try {
-        const decoded = jwt.verify(token, getJwtSecret()) as T;
-        return decoded;
-    } catch (err) {
-        return null;
-    }
+  try {
+    const decoded = jwt.verify(token, getJwtSecret()) as T;
+    return decoded;
+  } catch (err) {
+    return null;
+  }
 };
